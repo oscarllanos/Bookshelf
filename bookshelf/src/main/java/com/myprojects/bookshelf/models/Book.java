4589341d18f0a2e0e45d5e167660b6bdb73248e1
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-08-27 08:00:34 -0500

    Update Book.java

--- bookshelf/src/main/java/com/myprojects/bookshelf/models/Book.java
+++ bookshelf/src/main/java/com/myprojects/bookshelf/models/Book.java
@@ -1,4 +1,26 @@
 package com.myprojects.bookshelf.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Book {
+
+    @Getter @Setter
+    private Long id;
+
+    @Getter @Setter
+    private String title;
+
+    @Getter @Setter
+    private String author;
+
+    @Getter @Setter
+    private String description;
+
+    @Getter @Setter
+    private boolean read;
+
+    @Getter @Setter
+    private boolean inBookshelf;
+
 }
