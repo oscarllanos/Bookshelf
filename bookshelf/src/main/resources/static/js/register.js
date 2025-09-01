16435e371a5cc2a72912df4e6183b34a0a2b3758
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-09-01 07:50:47 -0500

    Update register.js

---
+++ bookshelf/src/main/resources/static/js/register.js
@@ -0,0 +1,27 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+// on ready
+});
+
+async function registerBook(){
+
+let datos = {};
+  datos.title =document.getElementById('txtTitle').value;
+  datos.author =document.getElementById('txtAuthor').value;
+  datos.description =document.getElementById('txtDescription').value;
+  datos.read =document.getElementById('txtRead').value;
+  datos.inBookshelf =document.getElementById('txtInBookshelf').value;
+
+
+      const request = await fetch('api/books', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        //body: JSON.stringify({a: 1, b: 'Textual content'})
+      });
+      const books = await request.json();
+
+
+}
