7316856909b7e7d89de5251b11fedcf0726c76fd
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-09-03 06:23:05 -0500

    Update register.js

--- bookshelf/src/main/resources/static/js/register.js
+++ bookshelf/src/main/resources/static/js/register.js
@@ -19,7 +19,7 @@ let datos = {};
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        //body: JSON.stringify({a: 1, b: 'Textual content'})
+        body: JSON.stringify(datos)
       });
       const books = await request.json();
 
