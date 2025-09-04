b6e1825267bd98e9e0d31b95b5cfc6b791dc885f
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-09-04 07:28:12 -0500

    Update User.java

--- bookshelf/src/main/java/com/myprojects/bookshelf/models/User.java
+++ bookshelf/src/main/java/com/myprojects/bookshelf/models/User.java
@@ -1,4 +1,32 @@
 package com.myprojects.bookshelf.models;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Table(name = "users")
+@ToString @EqualsAndHashCode
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "id")
+    private Long id;
+
+    @Getter @Setter @Column(name = "Nombre")
+    private String nombre;
+
+    @Getter @Setter @Column(name = "Apellido")
+    private String apellido;
+
+    @Getter @Setter @Column(name = "Email")
+    private String email;
+
+    @Getter @Setter @Column(name = "Password")
+    private String password;
+
 }
