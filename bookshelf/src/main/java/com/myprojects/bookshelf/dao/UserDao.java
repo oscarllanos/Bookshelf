124c1d21403843c26d1f86cb60e01566be71fcb8
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-09-05 06:39:10 -0500

    Update UserDao.java

--- bookshelf/src/main/java/com/myprojects/bookshelf/dao/UserDao.java
+++ bookshelf/src/main/java/com/myprojects/bookshelf/dao/UserDao.java
@@ -1,8 +1,7 @@
 package com.myprojects.bookshelf.dao;
 
-import com.myprojects.bookshelf.models.Book;
 import com.myprojects.bookshelf.models.User;
 
 public interface UserDao {
-    void verificarCredenciales(User user);
+    boolean verificarCredenciales(User user);
 }
