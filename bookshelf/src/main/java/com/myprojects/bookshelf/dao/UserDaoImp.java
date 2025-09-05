4156b0d72c9c52b87623e880ada3ccf0e57c41da
oscarllanos <oscarllanosperdomo1995@gmail.com>
2025-09-05 06:39:05 -0500

    Update UserDaoImp.java

--- bookshelf/src/main/java/com/myprojects/bookshelf/dao/UserDaoImp.java
+++ bookshelf/src/main/java/com/myprojects/bookshelf/dao/UserDaoImp.java
@@ -1,9 +1,12 @@
 package com.myprojects.bookshelf.dao;
 
+import com.myprojects.bookshelf.models.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 @Repository
@@ -12,4 +15,14 @@ public class UserDaoImp implements UserDao{
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
+    public boolean verificarCredenciales(User user) {
+        String query = "From User WHERE email = :email AND password = :password";
+        List<User> lista = entityManager.createQuery(query)
+                .setParameter("email", user.getEmail())
+                .setParameter("password", user.getPassword())
+                .getResultList();
+
+        return !lista.isEmpty();
+    }
 }
